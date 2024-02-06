450b365bfa805251a586360622f0fffc28efa449
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-06 10:16:10 +0000

    Update heading on button click

--- section-1/video-2/first-app/src/App.js
+++ section-1/video-2/first-app/src/App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 function App() {
+    const [advice, setAdvice] = useState("");
+
     async function getAdvice() {
         const res = await fetch("https://api.adviceslip.com/advice");
         const data = await res.json();
-        console.log(data);
+        setAdvice(data.slip.advice);
+        console.log(data.slip.advice);
     }
 
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>{advice}</h1>
             <button onClick={getAdvice}>Get advice</button>
         </div>
     )
