3a030646defb860c4eed297b4e0343db6854d1ee
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-06 10:21:08 +0000

    Added counter

--- section-1/video-2/first-app/src/App.js
+++ section-1/video-2/first-app/src/App.js
@@ -2,18 +2,20 @@ import { useState } from "react";
 
 function App() {
     const [advice, setAdvice] = useState("");
+    const [count, setCount] = useState(0);
 
     async function getAdvice() {
         const res = await fetch("https://api.adviceslip.com/advice");
         const data = await res.json();
         setAdvice(data.slip.advice);
-        console.log(data.slip.advice);
+        setCount((c) => c + 1);
     }
 
     return (
         <div>
             <h1>{advice}</h1>
             <button onClick={getAdvice}>Get advice</button>
+            <p>You have read <strong>{count}</strong> pieces of advice</p>
         </div>
     )
 }
