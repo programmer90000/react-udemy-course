f8357d95263c46ec88099c49ee5a8d70eb27927f
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-12 13:21:03 +0000

    Updated React app

--- section-3/video-16/pizza-menu/src/App.js
+++ section-3/video-16/pizza-menu/src/App.js
@@ -2,9 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <h1>Hello React!</h1>
-    );
+    return (<h1>Hello React!</h1>);
 }
 
 export default App;
