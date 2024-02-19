6824a4a6d771c60a5188864273e15e3fcd681e33
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-19 18:26:35 +0000

    Added multiple products

--- section-5/video-33/pizza-menu/src/index.js
+++ section-5/video-33/pizza-menu/src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello React!</h1>
+    return (
+        <div>
+            <h1>Hello React!</h1>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    );
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+            <h2>Pizza Spinaci</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
