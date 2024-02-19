28acd328ef0146d9d7a78ab367947898559283fe
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-19 20:21:40 +0000

    Added styling to the page

--- section-5/video-33/pizza-menu/src/index.js
+++ section-5/video-33/pizza-menu/src/index.js
@@ -1,22 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
     return (
         <div>
-            <h1>Hello React!</h1>
-            <Pizza />
-            <Pizza />
-            <Pizza />
+            <Header />
+            <Menu />
+            <Footer />
         </div>
     );
+}
+
+function Header() {
+    const style = {};
+    return (
+        <header className="header">
+            <h1 style={style}>Fast React Pizza Co.</h1>
+        </header>
+    )
+}
+
+function Menu() {
+    return (
+        <main className="menu">
+            <h2>Our menu</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </main>
+    )
+}
+
+function Footer() {
+    return <footer className="footer">{new Date().toLocaleTimeString()} We're currently open</footer>;
 }
 
 function Pizza() {
     return (
         <div>
             <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h2>Pizza Spinaci</h2>
+            <h3>Pizza Spinaci</h3>
             <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         </div>
     )
