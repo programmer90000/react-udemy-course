a848540dad8b16851487401465b05a8d77bc6fa4
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-21 16:55:29 +0000

    Conditionally render menu

--- section-5/video-33/pizza-menu/src/index.js
+++ section-5/video-33/pizza-menu/src/index.js
@@ -30,25 +30,20 @@ function Menu() {
         <main className="menu">
             <h2>Our menu</h2>
             <ul className="pizzas">
-                {numPizzas > 0 && (pizzas.map(pizza => <Pizza pizzaObject={pizza} key={pizza.name} />))}
+                {numPizzas > 0 ? (pizzas.map(pizza => <Pizza pizzaObject={pizza} key={pizza.name} />)) : <p>We're still working on our menu. Please come back later :)</p>}
             </ul>
         </main>
     )
 }
 
 function Footer() {
-    const hour = new Date().getHours();
-    const openHour = 12;
     const closeHour = 22;
-    const isOpen = hour >= openHour && hour <= closeHour;
     return (
         <footer className="footer">
-            {isOpen && (
-                <div className="order">
-                    <p>We're open until {closeHour}:00. Come visit us or order online</p>
-                    <button className="btn">Order</button>
-                </div>
-            )}
+            <div className="order">
+                <p>We're open until {closeHour}:00. Come visit us or order online</p>
+                <button className="btn">Order</button>
+            </div>
         </footer>
     )
 }
