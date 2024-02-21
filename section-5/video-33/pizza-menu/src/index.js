8594f272b67cfe135e54ea7258af074fc5224c06
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-21 16:33:49 +0000

    Update text when shop open

--- section-5/video-33/pizza-menu/src/index.js
+++ section-5/video-33/pizza-menu/src/index.js
@@ -36,7 +36,16 @@ function Footer() {
     const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
-    return <footer className="footer"></footer>;
+    return (
+        <footer className="footer">
+            {isOpen && (
+                <div className="order">
+                    <p>We're open until {closeHour}:00. Come visit us or order online</p>
+                    <button className="btn">Order</button>
+                </div>
+            )}
+        </footer>
+    )
 }
 
 function Pizza(props) {
