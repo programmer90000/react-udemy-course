00afb8f8d1aaa921f6caf4e872f6f2b2bf0fe823
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-21 16:56:13 +0000

    Conditionally render footer text

--- section-5/video-33/pizza-menu/src/index.js
+++ section-5/video-33/pizza-menu/src/index.js
@@ -37,13 +37,18 @@ function Menu() {
 }
 
 function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
     const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
     return (
         <footer className="footer">
-            <div className="order">
-                <p>We're open until {closeHour}:00. Come visit us or order online</p>
-                <button className="btn">Order</button>
-            </div>
+            {isOpen ? (
+                <div className="order">
+                    <p>We're open until {closeHour}:00. Come visit us or order online</p>
+                    <button className="btn">Order</button>
+                </div>
+            ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
         </footer>
     )
 }
