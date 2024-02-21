e78bba3291a65aa2a8f1429fc3c220266cbf7a01
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-21 17:24:28 +0000

    Destructred hours object

--- section-5/video-33/pizza-menu/src/index.js
+++ section-5/video-33/pizza-menu/src/index.js
@@ -48,10 +48,10 @@ function Footer() {
     )
 }
 
-function Order(props) {
+function Order({ openHour, closeHour }) {
     return (
         <div className="order">
-            <p>We're open until {props.closeHour}:00. Come visit us or order online</p>
+            <p>We're open from {openHour}:00 until {closeHour}:00. Come visit us or order online</p>
             <button className="btn">Order</button>
         </div>
     )
