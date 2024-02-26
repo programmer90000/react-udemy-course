317e9cb37a80ebd8888739afb506a56aa95ec54d
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-26 17:47:07 +0000

    Update component on Previous button click

--- section-6/video-57/steps/src/App.js
+++ section-6/video-57/steps/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
     const [step, setStep] = useState(1);
 
     function handlePrevious() {
-        alert("Previous");
+        setStep(step - 1);
     }
 
     function handleNext() {
