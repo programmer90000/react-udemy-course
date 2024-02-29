80d64ac79c367e42561740b7ebc6a097d6ca7c19
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-29 10:12:54 +0000

    Created functions to update step and count

--- section-6/video-68/date-counter/src/App.js
+++ section-6/video-68/date-counter/src/App.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import "./style.css";
 
 function App() {
     return (
         <div className="App">
+            <Counter />
         </div>
     );
+}
+
+function Counter() {
+    const [step, setStep] = useState(0);
+    const [count, setCount] = useState(0);
+
+    function incrementStep() {
+        setStep(step => step + 1);
+    }
+
+    function decrementStep() {
+        if (step >= 1) {
+            setStep(step => step - 1);
+        }
+    }
+
+    function incrementCount() {
+        setCount(count => count + 1);
+    }
+
+    function decrementCount() {
+        setCount(count => count + 1);
+    }
 }
 
 export default App;
