45361bcc8b97c05c60f13c357e1095fcb86dde64
Abdul Rehman <abdulr568programming@outlook.com>
2024-03-05 16:27:36 +0000

    Added form

--- section-6/video-69/travel-list/src/App.js
+++ section-6/video-69/travel-list/src/App.js
@@ -20,10 +20,20 @@ function Logo() {
 }
 
 function Form() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-        </div>
+            <select>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+            </select>
+            <input type="text" placeholder="Item..."></input>
+            <button>Add</button>
+        </form>
     )
 }
 
