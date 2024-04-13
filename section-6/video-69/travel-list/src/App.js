27c0947636cae9e960c0d6cd29236418d053bfa1
Abdul Rehman <abdulr568programming@outlook.com>
2024-04-13 12:32:20 +0100

    Get the item name from React

--- section-6/video-69/travel-list/src/App.js
+++ section-6/video-69/travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
     { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,8 +22,11 @@ function Logo() {
 }
 
 function Form() {
+    const [description, setDescription] = useState("");
+
     function handleSubmit(e) {
         e.preventDefault();
+        console.log(e);
     }
     return (
         <form className="add-form" onSubmit={handleSubmit}>
@@ -29,7 +34,7 @@ function Form() {
             <select>
                 {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
             </select>
-            <input type="text" placeholder="Item..."></input>
+            <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}></input>
             <button>Add</button>
         </form>
     )
