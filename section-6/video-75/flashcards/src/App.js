e8b7ed040c80eb197b47b06138e88a56cadb38bb
Abdul Rehman <abdulr568programming@outlook.com>
2024-04-13 17:38:23 +0100

    Change between question and answer on click

--- section-6/video-75/flashcards/src/App.js
+++ section-6/video-75/flashcards/src/App.js
@@ -43,12 +43,16 @@ const questions = [
 ];
 
 function FlashCards() {
-    const [selectedId, setSelectedId] = useState(9103);
+    const [selectedId, setSelectedId] = useState(null);
+
+    function handleClick(id) {
+        setSelectedId(id !== selectedId ? id : null);
+    }
 
     return (
         <div className="flashcards">
             {questions.map((question) => (
-                <div key={question.id}>
+                <div key={question.id} onClick={() => handleClick(question.id)} className={question.id === selectedId ? "selected" : ""}>
                     <p>{question.id === selectedId ? question.answer : question.question}</p>
                 </div>
             ))}
