ebe9dae865a406470e926f9ce8e9d5da88775760
Abdul Rehman <abdulr568programming@outlook.com>
2024-04-14 10:07:48 +0100

    Previous date counter code

--- section-6/video-76/date-counter/src/App.js
+++ section-6/video-76/date-counter/src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import "./style.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Counter />
         </div>
     );
+}
+
+function Counter() {
+    // Define States
+    const [step, setStep] = useState(0);
+    const [count, setCount] = useState(0);
+
+    const [monthNames, setMonthNames] = useState([]);
+    const [dayNames, setDayNames] = useState([]);
+
+    const [day, setDay] = useState("");
+    const [date, setDate] = useState("");
+    const [month, setMonth] = useState("");
+    const [year, setYear] = useState("");
+    const [dateText, setDateText] = useState("");
+
+    // Set date as soon as page loads
+    useEffect(() => {
+        setDayNames(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]);
+        setMonthNames(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]);
+        const today = new Date();
+
+        today.setDate(today.getDate() + count);
+        setDay(dayNames[today.getDay()]);
+        setMonth(monthNames[today.getMonth()]);
+        setDate(today.getDate());
+        setYear(today.getFullYear());
+
+        if (count === 0) {
+            setDateText(`Today is ${day} ${month} ${date} ${year}`);
+        } else {
+            today.setDate(today.getDate() + count);
+            setDay(dayNames[today.getDay()]);
+            setMonth(monthNames[today.getMonth()]);
+            setDate(today.getDate());
+            setYear(today.getFullYear());
+            setDateText(`${count} days from now is ${day} ${month} ${date} ${year}`);
+        }
+    })
+
+    // Functions to increment and decrement the step and count values
+    function incrementStep() {
+        setStep(step => step + 1);
+    }
+
+    function decrementStep() {
+        if (step > 1) {
+            setStep(step => step - 1);
+        }
+    }
+
+    function incrementCount() {
+        setCount(count => count + step);
+    }
+
+    function decrementCount() {
+        if (count >= step) {
+            setCount(count => count - step);
+        }
+    }
+
+    return (
+        <div>
+            <div><button onClick={decrementStep}>-</button> <span>Step: {step}</span> <button onClick={incrementStep}>+</button></div>
+            <br />
+            <div><button onClick={decrementCount}>-</button> <span>Count: {count}</span> <button onClick={incrementCount}>+</button></div>
+            <br />
+            <div>{dateText}</div>
+        </div>
+    )
 }
 
 export default App;
